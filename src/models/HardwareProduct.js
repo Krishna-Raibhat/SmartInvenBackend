deda2f3dd25969c535a6b8b24174b3744ead37ea
RawalAnuma <rawalanuma@gmail.com>
2025-12-15 23:28:10 +0545

    imported owner model

--- src/models/HardwareProduct.js
+++ src/models/HardwareProduct.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
+const Owner = require("./Owner");
 
 const HardwareProduct = sequelize.define(
   "HardwareProduct",
