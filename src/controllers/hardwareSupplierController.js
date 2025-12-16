fd9f362536e612aaaa4c24c5dc4f3f65e1c86dad
RawalAnuma <rawalanuma@gmail.com>
2025-12-16 16:09:44 +0545

    add read all and read by id hardware supplier controllers

--- src/controllers/hardwareSupplierController.js
+++ src/controllers/hardwareSupplierController.js
@@ -9,3 +9,29 @@ exports.createSupplier = async (req, res) => {
     res.status(500).json({ success: false, message: error.message });
   }
 };
+
+// READ ALL
+exports.getSuppliers = async (req, res) => {
+  try {
+    const { owner_id } = req.query;
+    const suppliers = await supplierService.getAllSuppliers(owner_id);
+    res.status(200).json({ success: true, data: suppliers });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+//READ BY ID
+exports.getSupplierById = async (req, res) => {
+  try {
+    const supplier = await supplierService.getSupplierById(req.params.id);
+    if (!supplier) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Supplier not found" });
+    }
+    res.status(200).json({ success: true, data: supplier });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
