ec290a01d8ba142bcf2b2687caac5bb4ab3ebb01
Aayush <aayushbastola772@gmail.com>
2025-12-16 23:56:13 +0545

    routes for product crud

--- src/routes/hardProdRoute.js
+++ src/routes/hardProdRoute.js
@@ -6,9 +6,13 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 
-router.get("/",authMiddleware,productController.getAll);
+router.get("/all",authMiddleware,productController.getAll);
 router.post("/newProduct",authMiddleware,productController.addNewProduct);
 router.put("/addUnit",authMiddleware,productController.addProductQuantity);
 router.put("/reduceUnit",authMiddleware,productController.reduceProductQuantity);
+router.get("/:product_id",authMiddleware,productController.getProductById);
+router.put("/update",authMiddleware,productController.updateProduct);
+
+router.delete("/:product_id",authMiddleware,productController.deleteProduct);
 
 module.exports=router;
