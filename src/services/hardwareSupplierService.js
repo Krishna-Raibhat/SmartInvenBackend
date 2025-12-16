cd2664850d9002403a05ef31d3d276d380cc7688
RawalAnuma <rawalanuma@gmail.com>
2025-12-16 15:48:10 +0545

    add read all and read by id for hardware supplier

--- src/services/hardwareSupplierService.js
+++ src/services/hardwareSupplierService.js
@@ -1,11 +1,21 @@
 const HardwareSupplier = require("../models/HardwareSupplier");
 
 class HardwareSupplierService {
+  // CREATE
+  async createSupplier(data) {
+    return await HardwareSupplier.create(data);
+  }
 
-    // CREATE
-    async createSupplier(data){
-        return await HardwareSupplier.create(data);
-    }
+  // READ ALL (optional owner filter)
+  async getAllSuppliers(owner_id) {
+    const condition = owner_id ? { where: { owner_id } } : {};
+    return await HardwareSupplier.findAll(condition);
+  }
 
-    
+  // READ BY ID
+  async getSupplierById(id) {
+    return await HardwareSupplier.findByPk(id);
+  }
 }
+
+module.exports = new HardwareSupplierService();
