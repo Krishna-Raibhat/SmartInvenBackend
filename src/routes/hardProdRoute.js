a420d9d8f6fe833602d96f3d2e9bbade65a542bf
Aayush <aayushbastola772@gmail.com>
2025-12-16 20:49:28 +0545

    corrected routes

--- src/routes/hardProdRoute.js
+++ src/routes/hardProdRoute.js
@@ -6,9 +6,9 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 
-router.get("/",authMiddleware,productController.getAllProducts);
+router.get("/",authMiddleware,productController.getAll);
 router.post("/newProduct",authMiddleware,productController.addNewProduct);
 router.put("/addUnit",authMiddleware,productController.addProductQuantity);
-router.put("/reduceUnit",authMiddleware,productController.reduceProductQuantity)
+router.put("/reduceUnit",authMiddleware,productController.reduceProductQuantity);
 
 module.exports=router;
