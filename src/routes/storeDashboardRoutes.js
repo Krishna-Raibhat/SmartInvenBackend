011c626fa8237c8d5055588b2936487c1ced5717
Krishna-Raibhat <krishnarb08@gmail.com>
2026-06-14 17:28:33 +0545

    update

--- src/routes/storeDashboardRoutes.js
+++ src/routes/storeDashboardRoutes.js
@@ -1,11 +1,12 @@
 // src/routes/storeDashboardRoutes.js
 import express from "express";
 import auth from "../middlewares/authMiddleware.js";
-import { getDashboard, getRecentActivities , getInventoryValue} from "../controllers/storeDashboardController.js";
+import { getDashboard, getRecentActivities , getInventoryValue, getLowStockItems } from "../controllers/storeDashboardController.js";
 
 const router = express.Router();
 
 router.get("/", auth, getDashboard);
 router.get("/activities", auth, getRecentActivities);
 router.get("/inventory-value", auth, getInventoryValue); 
+router.get("/low-stock", auth, getLowStockItems);
 export default router;
